163fa379dd9f54df471cbea60f73d30cfd150aa5
gangon <ekdldktnekf@gmail.com>
2023-02-26 14:41:01 +0900

    #0.1 bottomNavigationBar
    1. bottom navigation을 native로 사용할려고했는데 문제가 상당히 많은것으로 확인이 됨
    2. 그래서 구글링 하여 제일 괜찮아 보이는 블로그를 참고하여 적용 ( 아직 내 지식으로는 이해가 힘들어서 지금은 적용시켜두고 다른것을 먼저 공부)
    3. 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,152 +1,88 @@
-import 'package:aeffaff/widgets/button.dart';
-import 'package:aeffaff/widgets/currency_card.dart';
 import 'package:flutter/material.dart';
-
-class Player {
-  String? name;
-
-  Player({this.name});
-}
+import 'package:toonflix/utils/tab_item.dart';
+import 'package:toonflix/utils/tab_navigator.dart';
+import 'package:toonflix/widgets/bottom_navigation.dart';
 
 void main() {
-  var player = Player(name: 'Coxe');
-
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
-  App({super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 40,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          'Welcome, back',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                Text(
-                  'Total Balance',
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
-                    fontSize: 22,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  '\$5 179 482',
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.white.withOpacity(0.8),
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Button(
-                      text: 'Transfer',
-                      fontColor: Colors.black,
-                      backgroundColor: Color(0xFFE75480),
-                    ),
-                    Button(
-                      text: 'Request',
-                      fontColor: Color(0xFFE75480),
-                      backgroundColor: Color(0xFF1F2123),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CurrencyCard(
-                  name: 'Euro',
-                  code: 'EUR',
-                  amonut: '6 483',
-                  icon: Icons.euro_rounded,
-                  isInverted: false,
-                ),
-                const CurrencyCard(
-                  name: 'Bitcoin',
-                  code: 'BTC',
-                  amonut: '9 783',
-                  icon: Icons.currency_bitcoin_rounded,
-                  isInverted: true,
-                  offset: Offset(0, -40),
-                ),
-                const CurrencyCard(
-                  name: 'Dallar',
-                  code: 'USD',
-                  amonut: '428',
-                  icon: Icons.attach_money_outlined,
-                  isInverted: false,
-                  offset: Offset(0, -80),
-                ),
-              ],
-            ),
-          ),
+    return const MaterialApp(
+      home: RoutePage(),
+    );
+  }
+}
+
+class RoutePage extends StatefulWidget {
+  const RoutePage({super.key});
+
+  @override
+  State<RoutePage> createState() => _RoutePageState();
+}
+
+class _RoutePageState extends State<RoutePage> {
+  TabItem _currentTab = TabItem.home;
+
+  final _navigatorKeys = {
+    TabItem.home: GlobalKey<NavigatorState>(),
+    TabItem.wallets: GlobalKey<NavigatorState>(),
+    TabItem.state: GlobalKey<NavigatorState>(),
+  };
+
+  void _selectTab(TabItem tabItem) {
+    if (tabItem == _currentTab) {
+      /// 네비게이션 탭을 누르면, 해당 네비의 첫 스크린으로 이동!
+      _navigatorKeys[tabItem]!.currentState!.popUntil((route) => route.isFirst);
+    } else {
+      setState(() => _currentTab = tabItem);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab =
+            !await _navigatorKeys[_currentTab]!.currentState!.maybePop();
+
+        if (isFirstRouteInCurrentTab) {
+          // 메인화면 x
+          if (_currentTab != TabItem.home) {
+            _selectTab(TabItem.home);
+
+            return false;
+          }
+        }
+
+        return true;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            _buildOffstageNavigator(TabItem.home),
+            _buildOffstageNavigator(TabItem.wallets),
+            _buildOffstageNavigator(TabItem.state),
+          ],
         ),
+        bottomNavigationBar: BottomNavigation(
+          currentTab: _currentTab,
+          onSelectTab: _selectTab,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOffstageNavigator(TabItem tabItem) {
+    return Offstage(
+      offstage: _currentTab != tabItem,
+      child: TabNavigator(
+        navigatorKey: _navigatorKeys[tabItem],
+        tabItem: tabItem,
       ),
     );
   }
