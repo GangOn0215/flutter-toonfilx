31291bce6f6353d3dde03624decc79d670ae4223
gangon <ekdldktnekf@gmail.com>
2023-03-03 01:02:17 +0900

    #6.8 Webtoon Card

--- lib/screens/toonflix_screen.dart
+++ lib/screens/toonflix_screen.dart
@@ -10,35 +10,79 @@ class ToonflixScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Toonflix',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-            ),
+      appBar: AppBar(
+        title: const Text(
+          'Toonflix',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
           ),
-          centerTitle: true,
         ),
-        body: FutureBuilder(
-          future: webtoon,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  var webtoonRow = snapshot.data![index];
-
-                  return Text(webtoonRow.title);
-                },
-                separatorBuilder: (context, index) => const SizedBox(
-                  width: 10,
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: webtoon,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-              );
-            }
+                Expanded(child: makeList(snapshot))
+              ],
+            );
+            // return makeList(snapshot);
+          }
 
-            return const Text('Loading');
-          },
-        ));
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<ToonflixModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var webtoonRow = snapshot.data![index];
+
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  10,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+                ],
+              ),
+              child: Image.network(webtoonRow.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoonRow.title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
+      ),
+    );
   }
 }
